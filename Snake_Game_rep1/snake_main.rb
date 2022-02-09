686eeb4a2442c3523aa71928e389759619785802
Szymek_laptop_Acer <szymon.pawel.szymanski@gmail.com>
2022-02-09 17:47:53 +0100

    game screen, snake, apple appearing

---
+++ Snake_Game_rep1/snake_main.rb
@@ -0,0 +1,92 @@
+require 'ruby2d'
+
+set background: '#9b0a38'
+set width: 1020 #1020/20 = 51
+set height: 720 #720/20 = 36
+set fps_cap: 15
+GRID = 20
+GRID_W = Window.width / GRID
+GRID_H = Window.height / GRID
+
+class Snake
+  attr_writer :which_way
+  def initialize
+    @coordinates = [[2,0], [2,1], [2,2], [2,3]]
+    @which_way = 'down'
+  end
+
+  def draw
+    @coordinates.each do |current_pos|
+      Square.new(x: current_pos[0] * GRID, y: current_pos[1] * GRID, size: GRID - 1, color: 'white')
+    end
+  end
+
+  def move
+    @coordinates.shift
+    case @which_way
+    when 'down'
+      @coordinates.push(cords_mod(head_pos[0], head_pos[1] + 1))
+    when 'up'
+      @coordinates.push(cords_mod(head_pos[0], head_pos[1] - 1))
+    when 'right'
+      @coordinates.push(cords_mod(head_pos[0] + 1, head_pos[1]))
+    when 'left'
+      @coordinates.push(cords_mod(head_pos[0] - 1, head_pos[1]))
+    end
+  end
+
+  def can_change_way(new_way)
+    case @which_way
+    when 'down' then new_way != 'up'
+    when 'up' then new_way != 'down'
+    when 'right' then new_way != 'left'
+    when 'left' then new_way != 'right'
+    end
+  end
+
+  private
+
+  def cords_mod(x, y)
+    [x % GRID_W, y % GRID_H]
+  end
+
+  def head_pos
+    @coordinates.last
+  end
+
+end
+
+class Game_elements
+  def initialize
+    @points = 0
+    @apple_x = rand(GRID_W)
+    @apple_y = rand(GRID_H)
+  end
+
+  def draw
+    #Circle.new(x: @apple_x * GRID, y: @apple_y * GRID, radius: (GRID/2), sectors: 32, color: '#b7b0b0')
+    Square.new(x: @apple_x * GRID, y: @apple_y * GRID, size: GRID - 1, color: '#b7b0b0')
+  end
+end
+
+content = Game_elements.new
+wezyk = Snake.new
+
+update do
+  clear
+  content.draw
+  wezyk.draw
+  wezyk.move
+end
+
+on :key_down do |event|
+  puts event.key
+  if ['down', 'up', 'right', 'left'].include?(event.key)
+    if wezyk.can_change_way(event.key)
+      wezyk.which_way = event.key
+    end
+  end
+end
+
+show
+
